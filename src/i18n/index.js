265a1a46ee90bdc19e1ef20ca94aa1283b8c4808
coolxuefeng <coolxuefeng@qq.com>
2020-03-25 15:16:01 +0800

    feat: add i18n

---
+++ src/i18n/index.js
@@ -0,0 +1,82 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import initMessage from './lang/en-US';
+
+Vue.use(VueI18n);
+
+const savedLanguageKey = 'DEFAULT_LANGUAGE';
+const initLanguage = 'en-US';
+
+const dateTimeFormats = {
+  'en-US': {
+    short: {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    },
+    long: {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      weekday: 'short',
+      hour: 'numeric',
+      minute: 'numeric',
+    },
+  },
+  'zh-CN': {
+    short: {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    },
+    long: {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      weekday: 'short',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    },
+  },
+};
+
+export const i18n = new VueI18n({
+  locale: initLanguage,
+  fallbackLocale: initLanguage,
+  dateTimeFormats,
+  messages: {
+    [initLanguage]: initMessage,
+  },
+});
+
+const loadedLanguages = [initLanguage];
+
+function setI18nLanguage(lang) {
+  i18n.locale = lang;
+  document.querySelector('html').setAttribute('lang', lang);
+  localStorage.setItem(savedLanguageKey, lang);
+  return lang;
+}
+
+export function loadLanguageAsync(lang) {
+  if (i18n.locale !== lang) {
+    if (!loadedLanguages.includes(lang)) {
+      return import(
+        /* webpackChunkName: "lang-[request]" */ `./lang/${lang}`
+      ).then((msgs) => {
+        i18n.setLocaleMessage(lang, msgs.default);
+        loadedLanguages.push(lang);
+        return setI18nLanguage(lang);
+      });
+    }
+    return Promise.resolve(setI18nLanguage(lang));
+  }
+  return Promise.resolve(lang);
+}
+
+// restore default language
+const savedLanguage = localStorage.getItem(savedLanguageKey) || initLanguage;
+loadLanguageAsync(savedLanguage);
+
+export default i18n;
