5eee4e101ea966b6364abf59bf711e52be91f647
coolxuefeng <coolxuefeng@qq.com>
2020-04-04 11:42:55 +0800

    chore(prettier): fix Semicolons option

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
-  Semicolons: true,
+  semi: true,
   singleQuote: true,
   quoteProps: 'as-needed',
   jsxSingleQuote: false,
